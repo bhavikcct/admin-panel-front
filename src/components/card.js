38ca6167be0c29a5fcb8c47e0f880f2a09c33a80
bhavikcct <bhavik.m@communicationcrafts.com>
2025-05-15 16:52:17 +0530

    remove:js file

--- src/components/card.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export const Card = ({ children, className = "" }) => {
-    return (_jsx("div", { className: `bg-white rounded-2xl shadow p-4 ${className}`, children: children }));
-};
--- src/pages/estimation.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import EstimateForm from '@/components/estimationform';
-const EstimationPage = () => {
-    return (_jsx(EstimateForm, {}));
-};
-export default EstimationPage;
